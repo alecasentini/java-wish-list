215b71ba1c566b03080bf3b6dd04b1053be949cf
alecasentini <ale.casentini@gmail.com>
2023-09-21 12:49:49 +0200

    Esercizio 1 completato

--- src/org/java/list/List.java
+++ src/org/java/list/List.java
@@ -1,5 +1,34 @@
 package org.java.list;
 
-public class List {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class List {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> regali = new ArrayList<String>();
+
+        while (true) {
+            System.out.print("Inserisci un regalo da aggiungere alla lista: ");
+            String regalo = sc.nextLine();
+
+            regali.add(regalo);
+            
+            System.out.println("Numero di regali in lista: " + regali.size());
+
+            System.out.print("Vuoi aggiungere un altro regalo alla lista? (Y/n): ");
+            String risposta = sc.nextLine();
+
+            if (risposta.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+
+        System.out.println("Ecco la lista dei regali:");
+        for (String regalo : regali) {
+            System.out.println(regalo);
+        }
+    }
 }
