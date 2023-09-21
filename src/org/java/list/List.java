a0279be127baf435ec0a9f7733311d43c3abd4fe
alecasentini <ale.casentini@gmail.com>
2023-09-21 14:02:41 +0200

    Esercizio 1 + Bonus

--- src/org/java/list/List.java
+++ src/org/java/list/List.java
@@ -8,13 +8,19 @@ public class List {
 
         Scanner sc = new Scanner(System.in);
 
-        ArrayList<String> regali = new ArrayList<String>();
+        ArrayList<String> regali  = new ArrayList<String>();
+        ArrayList<String> destinatari = new ArrayList<String>();
+
 
         while (true) {
             System.out.print("Inserisci un regalo da aggiungere alla lista: ");
             String regalo = sc.nextLine();
-
             regali.add(regalo);
+            
+            System.out.print("Inserisci il destinatario del regalo: ");
+            String destinatario = sc.nextLine();
+            destinatari.add(destinatario);
+
             
             System.out.println("Numero di regali in lista: " + regali.size());
 
@@ -27,8 +33,10 @@ public class List {
         }
 
         System.out.println("Ecco la lista dei regali:");
-        for (String regalo : regali) {
-            System.out.println(regalo);
+        for (int i = 0; i < regali.size(); i++) {
+            String regalo = regali.get(i);
+            String destinatario = destinatari.get(i);
+            System.out.println((i + 1) +") " + regalo + " per " + destinatario);
         }
     }
 }
